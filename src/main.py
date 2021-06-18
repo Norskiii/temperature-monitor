e0b2291e3c3949e5dd7bfb4c423c397445cb73a2
Norskiii <noora.poysti@outlook.com>
2021-06-18 21:07:28 +0300

    add api call for temperature observations

--- src/main.py
+++ src/main.py
@@ -9,13 +9,18 @@ sense = SenseHat()
 sense.low_light = True
 
 
-def getTimeFrame():
+def getTimeFrames():
         time = datetime.now() + timedelta(hours=1)
-        start = time.strftime("%Y-%m-%dT%H:00:00Z")
+        forecast_start = time.strftime("%Y-%m-%dT%H:00:00Z")
         time = time + timedelta(hours=23)
-        end = time.strftime("%Y-%m-%dT%H:00:00Z")
+        forecast_end = time.strftime("%Y-%m-%dT%H:00:00Z")
 
-        return start, end
+        time = datetime.now() + timedelta(hours=1)
+        observation_end = time.strftime("%Y-%m-%dT%H:00:00Z")
+        time = time = time - timedelta(hours=23)
+        observation_start = time.strftime("%Y-%m-%dT%H:00:00Z")
+
+        return observation_start, observation_end, forecast_start, forecast_end
 
 
 def readTemperature():
@@ -23,55 +28,31 @@ def readTemperature():
 
 
 def showWarning(colour):
-    x = colour
-    o = (0, 0, 0)
+    end_time = datetime.now() + timedelta(hours=1)
 
-    warning = [o, o, o, x, x, o, o, o,
-            o, o, o, x, x, o, o, o,
-            o, o, o, x, x, o, o, o,
-            o, o, o, x, x, o, o, o,
-            o, o, o, x, x, o, o, o,
-            o, o, o, o, o, o, o, o,
-            o, o, o, x, x, o, o, o,
-            o, o, o, x, x, o, o, o]
-    sense.set_pixels(warning)
-
-
-def allOK():
-    x = (0, 255, 0)
-    o = (0, 0, 0)
-
-    ok = [o, o, o, o, o, o, o, o,
-            x, x, x, o, x, o, o, x,
-            x, o, x, o, x, o, x, o,
-            x, o, x, o, x, x, o, o,
-            x, o, x, o, x, o, x, o,
-            x, x, x, o, x, o, o, x,
-            o, o, o, o, o, o, o, o,
-            x, x, x, x, x, x, x, x]
-
-    sense.set_pixels(ok)
+    while end_time > datetime.now():
+        sense.show_message("!!!", text_colour = colour)
 
 
 def main():
     i = 0
-    values = np.full(24, 15)
+    sensor_values = np.zeros(24)
 
     while True:
-        sense.clear()
         yellow_warning = False
         red_warning = False
-        start, end = getTimeFrame()
+        observation_start, observation_end, forecast_start, forecast_end = getTimeFrames()
 
-        # Temeprature forecast and current reading
-        times, forecast_values = apiRequest("Tampere", start, end)
-        values[i] = readTemperature()
+        # Temperature forecast, observation and current reading
+        forecast_times, forecast_values = apiRequestNext24H("Tampere", forecast_start, forecast_end)
+        observation_times, observation_values = apiRequestLast24H("Tampere", observation_start, observation_end)
+        sensor_values[i] = readTemperature()
         
         for temperature in forecast_values:
             if float(temperature) < 10:
                 yellow_warning = True
         
-        for temperature in values:
+        for temperature in sensor_values:
             if temperature < 15:
                 red_warning = True
                 
@@ -80,9 +61,8 @@ def main():
         elif red_warning:
             showWarning([255, 0, 0])
         else:
-            allOK()
+            time.sleep(3600)
 
-        time.sleep(3600)
         i += 1
         i = i % 24
 
--- src/weather_api.py
+++ src/weather_api.py
@@ -1,12 +1,22 @@
 import requests
 import xml.etree.ElementTree as ET
-from datetime import datetime, timedelta
 
-def apiRequest(location, start, end):
+def apiRequestNext24H(location, start, end):
     url = "https://opendata.fmi.fi/wfs?request=getFeature&version=2.0.0&storedquery_id=fmi::forecast::hirlam::surface::point::simple&&place=" + \
           location + "&timestep=60&starttime=" + \
           start + "&endtime=" + \
           end + "&parameters=temperature"
+
+    r = requests.get(url)
+    
+    return parseXML(r.text)
+
+
+def apiRequestLast24H(location, start, end):
+    url = "https://opendata.fmi.fi/wfs?request=getFeature&version=2.0.0&storedquery_id=fmi::observations::weather::hourly::simple&&place=" + \
+          location + "&timestep=60&starttime=" + \
+          start + "&endtime=" + \
+          end + "&parameters=TA_PT1H_AVG"
 
     r = requests.get(url)
     
@@ -23,3 +33,23 @@ def parseXML(content):
         values.insert(i, root[i][0][3].text)
 
     return times, values
+
+
+def main():
+    times1, values1 = apiRequestLast24H("Tampere", "2021-06-17T20:00:00Z", "2021-06-18T19:00:00Z")
+    times2, values2 = apiRequestNext24H("Tampere", "2021-06-18T20:00:00Z", "2021-06-19T19:00:00Z")
+
+    print("Last 24H:")  
+    for i in range(0, 24):
+        print("Time:", times1[i])
+        print("Temp: ", values1[i])
+
+
+    print("Next 24H:")  
+    for i in range(0, 24):
+        print("Time:", times2[i])
+        print("Temp: ", values2[i])
+
+
+if __name__ == "__main__":
+    main()
